18345ff2614b418c7bc3336b16cdc119a8a8c039
Sam Haycraft <phreakdawg@gmail.com>
2014-08-06 16:48:48 -0600

    asks user for number of iterations, calculates fibonacci numbers and last resultant ratio.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 __author__ = 'Sam Haycraft'
+
+num = int(input('Enter number of iterations: '))
+
+fibs = [1, 1]
+for i in range(2, num):
+    fibs.append(fibs[i-1]+fibs[i-2])
+
+print fibs
+print 'ratio = ' + str(float(fibs.pop())/float(fibs.pop()))
