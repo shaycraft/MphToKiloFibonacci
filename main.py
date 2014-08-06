c9d354b6969113e3da7e97946648fc3bb9d46f2d
Sam Haycraft <phreakdawg@gmail.com>
2014-08-06 17:01:53 -0600

    calculates conversion for kilometers in actual value,and by using golden ratio, then calculates the difference.

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ for i in range(2, num):
     fibs.append(fibs[i-1]+fibs[i-2])
 
 print fibs
-print 'ratio = ' + str(float(fibs.pop())/float(fibs.pop()))
+ratio = float(fibs.pop())/float(fibs.pop())
+print 'ratio = ' + str(ratio)
+
+kilo = float(input('Enter kilometers: '))
+miles_actual = kilo * 1.60934
+miles_fib = kilo * ratio
+diff = abs(miles_actual - miles_fib)
+
+print 'Actual miles = ' + str(miles_actual)
+print 'Calculated miles with golden ration= ' + str(miles_fib)
+print 'Residual = ' + str(diff)
